d4aa8a02169cd6c50fea2b29c80b9c247dd73051
dkurmaev <danilkurmayev@gmail.com>
2024-12-28 17:19:07 +0100

    refactoring

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "animate.css";
 import ContactFormModal from "./ContactFormModal";
+import { BadgeCheckIcon } from "@heroicons/react/solid";
 
 const certificates = [
   {
@@ -30,7 +31,8 @@ const HomeSlider = () => {
     const costCalculatorSection = document.getElementById("cost-calculator");
     if (costCalculatorSection) {
       const offset = -200;
-      const elementPosition = costCalculatorSection.getBoundingClientRect().top + window.pageYOffset;
+      const elementPosition =
+        costCalculatorSection.getBoundingClientRect().top + window.pageYOffset;
       const offsetPosition = elementPosition + offset;
 
       window.scrollTo({
@@ -41,12 +43,14 @@ const HomeSlider = () => {
   };
 
   const handleNext = () => {
-    setCurrentCertificateIndex((prevIndex) => (prevIndex + 1) % certificates.length);
+    setCurrentCertificateIndex(
+      (prevIndex) => (prevIndex + 1) % certificates.length
+    );
   };
 
   const handlePrev = () => {
-    setCurrentCertificateIndex((prevIndex) =>
-      (prevIndex - 1 + certificates.length) % certificates.length
+    setCurrentCertificateIndex(
+      (prevIndex) => (prevIndex - 1 + certificates.length) % certificates.length
     );
   };
 
@@ -61,23 +65,33 @@ const HomeSlider = () => {
         backgroundPosition: "center top",
       }}
     >
-      <div className="absolute inset-0 bg-black bg-opacity-80"></div>
+      <div className="absolute inset-0 bg-gray-900 bg-opacity-90"></div>
 
       <div className="relative z-10 text-center md:my-24 lg:my-28 sm:my-20 py-5">
-        <h1 className="text-2xl sm:text-3xl md:text-6xl lg:text-7xl mt-24 font-heading font-bold text-gray-300 uppercase tracking-widest animate__animated animate__fadeIn animate__delay-1s">
-          Dachdeckerarbeiten jeder Komplexität schlüsselfertig.
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl mt-16 font-heading font-bold text-gray-300 uppercase tracking-widest animate__animated animate__fadeIn animate__delay-1s">
+          <span className="bg-gradient-to-r from-teal-500 to-yellow-500 bg-clip-text text-transparent">
+            Dach
+          </span>
+          deckerarbeiten jeder Komplexität schlüsselfertig.
         </h1>
-        <p className="text-sm sm:text-md md:text-xl lg:text-2xl font-sans text-yellow-400 italic mt-2 sm:mt-2 animate__animated animate__fadeIn animate__delay-3s">
+        <p className="text-sm sm:text-md md:text-xl lg:text-2xl font-sans text-yellow-400 italic mt-4 sm:mt-2 animate__animated animate__fadeIn animate__delay-3s">
           Wir bieten die besten Lösungen für Ihr Dach!
         </p>
-        <p
-          className="text-md sm:text-lg md:text-xl lg:text-2xl font-sans text-gray-400 mt-6 cursor-pointer underline hover:text-gray-200 transition"
-          onClick={toggleCertificateModal}
-        >
-          Zertifizierte Meisterbetrieb
-        </p>
 
-        <div className="flex flex-col md:flex-row items-center justify-center gap-4 w-full mt-8">
+        <div className="flex justify-center items-center gap-4 text-gray-400 animate__animated animate__fadeIn animate__delay-3s mt-6">
+          <span
+            className="text-md sm:text-lg md:text-xl lg:text-2xl font-sans text-gray-400 cursor-pointer underline hover:text-gray-200 transition"
+            onClick={toggleCertificateModal}
+          >
+            Zertifizierte Meisterbetrieb
+          </span>
+          <BadgeCheckIcon
+            className="h-12 w-12 text-teal-800 ml-2 cursor-pointer vibrateCertificate glowEffect"
+            onClick={toggleCertificateModal}
+          />
+        </div>
+
+        <div className="flex flex-col md:flex-row items-center justify-center gap-8 w-full mt-8">
           <button
             onClick={toggleContactModal}
             className="px-6 py-3 bg-teal-900 text-white text-xl font-bold uppercase rounded-xl hover:bg-teal-700 transition"
@@ -100,31 +114,31 @@ const HomeSlider = () => {
             className="absolute inset-0 bg-black bg-opacity-70 backdrop-blur-md"
             onClick={toggleCertificateModal}
           ></div>
-          <div className="relative p-0 rounded-lg w-full max-w-5xl max-h-[90vh] z-50"> {/* Ограничиваем ширину и высоту контейнера */}
-          <div className="relative w-full h-screen flex items-center justify-center">
+          <div className="relative rounded-lg w-full max-w-2xl max-h-[90vh] z-50 overflow-hidden">
+            <div className="relative w-full h-full flex items-center justify-center">
               <img
                 src={certificates[currentCertificateIndex].src}
                 alt={certificates[currentCertificateIndex].alt}
                 className="max-w-full max-h-full object-contain"
               />
               <button
-                className="absolute top-2 right-2 text-gray-700 hover:text-gray-400"
+                className="absolute top-0 right-0  bg-opacity-80 rounded-full p-2 hover:bg-opacity-100 transition duration-200"
                 onClick={toggleCertificateModal}
               >
                 <img
                   src="/images/close-icon.gif"
                   alt="close"
-                  className="w-24 h-24 "
+                  className="w-10 h-10"
                 />
               </button>
               <button
-                className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-teal-900 hover:bg-teal-600 text-white p-2 rounded-full"
+                className="absolute left-1 top-1/2 transform -translate-y-1/2 bg-teal-900 hover:bg-teal-600 text-white p-3 rounded-full transition duration-200"
                 onClick={handlePrev}
               >
                 &lt;
               </button>
               <button
-                className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-teal-900 hover:bg-teal-600 text-white p-2 rounded-full"
+                className="absolute right-1 top-1/2 transform -translate-y-1/2 bg-teal-900 hover:bg-teal-600 text-white p-3 rounded-full transition duration-200"
                 onClick={handleNext}
               >
                 &gt;
