adc0a6a829eb95add6b82b47b561956fe7afa1fe
dkurmaev <danilkurmayev@gmail.com>
2025-01-09 18:06:14 +0100

    commingsoon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import Error404 from './pages/Error404';
 import ComingSoon from './pages/ComingSoon';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        {/* <Route exact path="/" element={<Home />} /> */}
         <Route path="/coming-soon" element={<ComingSoon />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
